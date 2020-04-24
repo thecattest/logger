b06b1286145751dba2403c178e6e16f73d046bc0
vodopyanov ilya <vodopyanov999@gmail.com>
2020-04-24 11:59:53 +0300

    .gitignore + get_logger function + example

---
+++ get_logger.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def get_logger(fn, logger_name="converter"):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler(fn)
+    formatter = logging.Formatter('%(asctime)s - %(name)s.%(levelname)s: %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    return logger
+
+
+if __name__ == '__main__':
+    logger = get_logger("log1.log", "bot")
+    logger.info("Hell o world")
+    logger.error("You")
